17fd86eb086e12606e77a4349ba2afa1d552036c
harshparmar4760 <harshparmar4760@gmail.com>
2023-03-12 23:13:16 +0530

    save

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,19 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+const api_key = "da54df377cd6f531fd5861d847ba687e";
+
+/**
+ * fetch data from server
+ * @param {string} URL API url  
+ * @param {Function} callback callback 
+ */
+
+export const fetchData = function(URL, callback){
+    fetch('${URL}&appid=${api_key}')
+        .then(res => res.json())
+        .then(data => callback(data));
+}
