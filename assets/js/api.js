d43081a645e48c7f4a1837c021eb32c80d2b1bb6
harshparmar4760 <harshparmar4760@gmail.com>
2023-03-13 00:15:08 +0530

    save

--- assets/js/api.js
+++ assets/js/api.js
@@ -20,3 +20,24 @@ export const fetchData = function(URL, callback){
         .then(res => res.json())
         .then(data => callback(data));
 }
+
+export const url = {
+    currentweather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query search query e.g: "London", "New York"
+     */
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
