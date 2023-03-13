ba5f2f11062d0614d19d9278d05730f0039aba4d
harshparmar4760 <harshparmar4760@gmail.com>
2023-03-13 23:31:09 +0530

    save

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,66 @@ const addEventListener = function (elements, eventType, callback) {
 const searchView = document.querySelector("[data-search-view]");
 const searchTogglers = document.querySelectorAll("[data-search-toggler]");
 
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventListener(searchTogglers, "click", toggleSearch);
+
+/**
+ * Search Integration
+ */
+const searchField = document.querySelector("[data-search-field]");
+const searchResult = document.querySelector("[data-search-result]");
+
+let searchTimeout = null;
+const searchTimeoutDuration = 500;
+
+searchField.addEventListener("input", function(){
+
+    setTimeout ?? clearTimeout(searchTimeout);
+
+    if (!searchField.value){
+        searchResult.classList.remove("active");
+        searchResult.innerHTML= "";
+        searchField.classList.remove("searching")
+    } else {
+        searchField.classList.add("searching")
+    }
+
+    if (searchField.value){
+        searchTimeout = setTimeout(()=> {
+            fetchData(url.geo(searchField.value), function (locations){
+                searchField.classList.remove("searching");
+                searchResult.classList.add("active");
+                searchResult.innerHTML=`
+                    <ul class="view-list" data-search-list>
+
+                        <li class="view-item">
+                            
+                        </li>
+                    </ul>
+                `;
+
+                const /** {Nodelist} | [] */ items = [];
+
+                for (const {name, lat, lon, country, state} of locations) {
+                    const searchItem = document.createElement("li");
+                    searchItem.classList.add("view-item");
+
+                    searchItem.innerHTML = `
+                        <span class="material-symbols-outlined">location_on</span>
+                        
+                        <div>
+                            <p class="item-title">${name}</p>
+                            
+                            <p class="label-2 item-subtitle">${state || ""} ${country}</p>
+                        </div>
+                        
+                        <a href="#/weather?lat=${lat}&lon=${lon}" class="item-link has-state" aria-label="${name} weather" data-search-toggler></a>
+                    `;
+
+                searchResult.querySelector("data-search-list").appendChild (searchItem);
+                items.push(searchItem.querySelector("[data-search-toggler]"));
+                }
+            })
+        }, searchTimeoutDuration);
+    }
+})
